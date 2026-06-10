b1f07d20f82e152628ea8a5b0e39965656cb9aa9
Alxstudio <Alexcbstudio@gmail.com>
2026-06-10 08:20:51 +0200

    fix: supabase image domain

--- sedentaris-web/next.config.ts
+++ sedentaris-web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'frkztothyiycmlqvqkec.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
